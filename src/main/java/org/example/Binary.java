fd26f92182072b716b12cada697513cb2a040c8e
Zack Di Salvo <zdisalvo@gmail.com>
2023-10-17 00:02:22 -0700

    completed binary search program with iterative and recursion selection capability, program counts number of iterations and successfully returns the index of a searched for value or -1 if it does not exist.

--- src/main/java/org/example/Binary.java
+++ src/main/java/org/example/Binary.java
@@ -8,17 +8,57 @@ public class Binary {
 
     private int count = 0;
 
+    private int recursionStart;
+    private int recursionEnd;
+    private int recursionSearchIndex = -1;
+
     public Binary() {
     }
 
     public void createList(int size, int increment) {
-        for (int i = 0 ; i <= size ; i++) {
+        for (int i = 0 ; i < size ; i++) {
             searchList.add(increment + increment * i);
         }
     }
 
     public ArrayList<Integer> getSearchList() {
         return searchList;
+    }
+
+    public int findIntegerIndexRecursion(int number ) {
+        count++;
+
+        if (recursionSearchIndex == -1) {
+            recursionStart = 0;
+            recursionEnd = searchList.size() - 1;
+            recursionSearchIndex = (recursionEnd - recursionStart) / 2;
+        } else {
+            recursionSearchIndex = (recursionEnd - recursionStart) / 2;
+        }
+
+        //TODO - need to check for match with start and end
+        if (recursionEnd - recursionStart <= 1) {
+            if (searchList.get(recursionStart) == number) {
+                return recursionStart;
+            } else if (searchList.get(recursionEnd) == number) {
+                return recursionEnd;
+            } else {
+                return -1;
+            }
+        }
+
+        int result = searchList.get(recursionSearchIndex) - number;
+
+        if (result == 0) {
+            return recursionSearchIndex;
+        } else if (result > 0) {
+            recursionEnd = recursionSearchIndex;
+            return findIntegerIndexRecursion(number);
+        } else {
+            recursionStart = recursionSearchIndex;
+            return findIntegerIndexRecursion(number);
+        }
+
     }
 
     public int findIntegerIndex(int number) {
@@ -60,7 +100,10 @@ public class Binary {
         }
 
         return -1;
+
     }
+
+
 
     public int getCount() {
         return count;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,15 +15,44 @@ public class Main {
 
         System.out.println("Please enter a value to search for: ");
         int value = scanner.nextInt();
+        scanner.nextLine();
+
+        boolean validAPISelection;
+        String searchAPI = "";
+
+        do {
+            validAPISelection = true;
+            System.out.println("Would you like to use the ITERATIVE or RECURSION api?: ");
+            searchAPI = scanner.nextLine();
+            searchAPI = searchAPI.toUpperCase();
+            try {
+                SearchAPI.valueOf(searchAPI);
+            } catch (IllegalArgumentException e){
+                System.out.println("Invalid Selection");
+                validAPISelection = false;
+
+            }
+        } while (validAPISelection == false);
 
         Binary binary = new Binary();
 
         binary.createList(size, increment);
 
-        int indexFound = binary.findIntegerIndex(value);
+
+
+        int indexFound = -1;
+
+        if (SearchAPI.valueOf(searchAPI) == SearchAPI.ITERATIVE) {
+            indexFound = binary.findIntegerIndex(value);
+        }
+
+        if (SearchAPI.valueOf(searchAPI) == SearchAPI.RECURSION) {
+            indexFound = binary.findIntegerIndexRecursion(value);
+        }
+
 
         System.out.println(String.format("The value was found at index %d." +
-                "%nThis search took %d iteration(s).", indexFound, binary.getCount()));
+                "%nThis search took %d iteration(s) using %s.", indexFound, binary.getCount(), searchAPI));
 
     }
 }
